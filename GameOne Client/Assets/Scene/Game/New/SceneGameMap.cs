712d8ba99728012415ec568dd5939a15ea146696
azazaza <DogEsh@yandex.ru>
2016-06-07 20:40:04 +0600

    commit 1.0.6

--- GameOne Client/Assets/Scene/Game/New/SceneGameMap.cs
+++ GameOne Client/Assets/Scene/Game/New/SceneGameMap.cs
@@ -1,15 +1,18 @@
 ﻿using SimpleTeam.GameOne.GameInfo;
 using System.Collections.Generic;
+using System;
 
 namespace Assets.Scene.Game.New
 {
+    using GameObjID = UInt16;
     class SceneGameMap
     {
         private IMapInfo _mapInfo;
-        private Dictionary<>
+        private Dictionary<GameObjID, ISimplus> _simpluses = null;
         public void SetToInitMap(IMapInfo mapInfo)
         {
             _mapInfo = mapInfo;
+            ClearMap();
             InitMap();
             UpdateMap();
         }
@@ -17,14 +20,31 @@ namespace Assets.Scene.Game.New
         {
             _mapInfo = mapInfo;
         }
-        private void InitMap()
-        {
-
-        }
+      
         private void UpdateMap()
         {
 
         }
+        private void InitMap()
+        {
+            foreach(ISimplusInfo s in _mapInfo.GetContainerSimplus())
+            {
+                _simpluses.Add(s.ID,s);
+            }
+            
 
+
+        }
+        private void ClearMap()
+        {
+            if (_simpluses != null)
+            {
+                foreach (ISimplus s in _simpluses.Values)
+                {
+                    s.Destroy();
+                }
+            }
+            _simpluses = new Dictionary<GameObjID, ISimplus>();
+        }
     }
 }
---
+++ GameOne Client/Assets/Scene/Game/New/Simplus/ISimplus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SimpleTeam.GameOne.GameInfo;
+
+namespace Assets.Scene.Game.New
+{
+    interface ISimplus
+    {
+        void UpdateInfo(ISimplusInfo info);
+        void Destroy();
+
+    }
+}
